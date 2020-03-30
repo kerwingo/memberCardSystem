04d55e74d30c99ac87367b2df370c85a22c3a244
kerwingo <kerwin_1727@163.com>
2020-03-30 08:45:11 +0800

    new

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,9 +6,6 @@ Page({
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     PageCur: 'index',
     cardList:[
       {store:'金牛万达',inital:100,balance:50,cardId:625785445247851525},
@@ -41,6 +38,9 @@ Page({
     this.setData({
       PageCur: e.currentTarget.dataset.cur
     })
+    if(e.currentTarget.dataset.cur =='profile'){
+      this.selectComponent("#profile").getUserInfo()
+    }
   },
   //打开弹窗
   showModal(e) {
@@ -105,32 +105,7 @@ Page({
     request.ajax()
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    console.log(1)
   },
   getUserInfo: function(e) {
     console.log(e)
